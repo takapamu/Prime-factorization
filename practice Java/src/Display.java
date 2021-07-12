ce01510e9b5b6dbc8e72352e44958a9258b80c41
takapamu <takapamukyary0129@gmail.com>
2021-07-12 23:08:55 +0900

    add bottom_panel

--- practice Java/src/Display.java
+++ practice Java/src/Display.java
@@ -53,5 +53,25 @@ public class Display implements ActionListener
     setPanel(mid_panel, Color.CYAN, null, new Dimension(480, 180 ));
     disp.add( mid_panel, BorderLayout.CENTER );
 
+    setLabelFont(parent_lbl, Color.EHITE, 90, 10, 150, 20, 14, false);
+    setLabelFont(parent_suit_lbl, Color.WHITE, 100, 10, 80, 100, 16, false);
+    setLabelFont(parent_no_lbl, Color.WHITE, 100, 35, 80, 100, 16, true);
+
+    child_lbl = new JLabel("あなたのカード");
+    child_suit_lbl = new JLabel("");
+    child_no_lbl = new JLabel("？");
+
+    mid_panel.add(child_lbl);
+    mid_panel.add(child_suit_lbl);
+    mid_panel.add(child_no_lbl);
+
+    setLabelFont(child_lbl, Color.EHITE, 265, 10, 150, 20, 14, false);
+    setLabelFont(child_suit_lbl, Color.LIGHT_GRAY, 300, 10, 80, 100, 16, false);
+    setLabelFont(child_no_lbl, Color.LIGHT_GRAY, 300, 35, 80, 100, 16, true);
+
+    //ボトムパネルの表示設定
+    bottom_panel = new JPanel();
+    setPanel( bottom_panel, Color.LIGHT_GRAY, new BorderLayout(), new Dimension(480, 50));
+    disp.add(bottom_panel, BorderLayout.SOUTH);
   }
 }
